0f2a3eb8b826c98ed0829228d9678c371e795940
duyanh <duy-anh@mail.ru>
2022-03-20 16:24:34 +0300

    minor

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+app.use(bodyParser.json())
 app.use('/api/content', require('./routes/content.routes'))
 
 const start = async () => {
