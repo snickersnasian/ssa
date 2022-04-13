1901fa5cd248af22eceac06caf28d054cfe8ba16
Зуи Линь <NGUZUI@lan.rarus.ru>
2022-04-14 00:15:31 +0300

    Create ForStudent component.

---
+++ client/src/components/ForEveryStudent.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+
+
+export const ForEveryStudent = () => {
+
+
+    return(
+        <div className="for-student">
+            <div className="for-student__head">
+                <div className="for-student__title">
+                    КАЖДОМУ СТУДЕНТУ
+                </div>
+                <div className="for-student__sub-title">
+                    Мы про студентов, помогите придумать фразу!
+                </div>
+            </div>
+
+            <div className="for-student__cards">
+
+                <div className="for-student__card">
+                    <div className="for-student__image">
+
+                    </div>
+
+                    <div className="for-student__card-content">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
