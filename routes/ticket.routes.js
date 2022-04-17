efaccd0364f1da35e1d5c5d60fa890543437203f
duyanh <duy-anh@mail.ru>
2022-04-17 16:08:43 +0300

    Style mail ticket

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -11,6 +11,8 @@ router.post('/createTicket', async (req, res) => {
 
         const hostUrl = req.protocol + '://' + req.get('host')
 
+        const year  = new Date().getFullYear()
+
         if (Object.keys(req.body).length !== 6) {
             return res.status(400).json({
                 message: 'Error. Body not found or missed fields'
@@ -43,31 +45,34 @@ router.post('/createTicket', async (req, res) => {
         const qr = await QRCode.toDataURL(ticketUrl)
 
         const mailBody = 
-            `<!DOCTYPE html>
-            <html lang="en">
+            `
             <head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
-                <style>
-                    h1 {
-                        text-align: center;
-                    }
-                </style>
             </head>
-            <body>
-                <div className="message">
-                    <h1>Вы зарегистрированы на ${event}</h1> 
-                    <h2>Место: ${place}</h2> 
-                    <h2>Дата: ${time}</h2>
+            <body style="width: 600px;height: 800px;margin: 0 auto 50px;">
+                <div class="header" style="margin-bottom: 60px;">
+                    <div class="title" style="width: 374px;height: 60px;font-family: 'Helvetica Neue', sans-serif;font-style: italic;text-transform: uppercase;font-weight: 300;font-size: 30px;line-height: 30px;color:#BF0202;"><p >${event}</p></div>
+                    <div class="role" style="width: 300px;height: 42px;padding-left: 40px;text-transform: uppercase;font-family: 'Helvetica Neue', sans-serif;font-style: italic;font-weight: 300;font-size: 35px;line-height: 42px;"><p>${firstName} ${lastName}</p></div>
+                </div>
 
-                    <p>Имя: ${firstName}</p> 
-                    <p>Фамилия: ${lastName}</p>
-                    <img src=${qr}></img>
+                <div class="event" style="height: 43px;margin-bottom: 10px;font-family: 'Helvetica Neue', sans-serif;font-style: normal;font-weight: 700;font-size: 35px;line-height: 43px;text-align: center;letter-spacing: 0.05em;color: #000000;"><p>Отсканируйте QR</p></div>
+
+                <div class="qr" style="width: 329px;height: 329px;background: #898989;margin: 0 auto 70px;">
+                    <img style="width: 329px;height: 329px;" src=${qr}></img>
+                </div>
+
+                <div class="place" style="margin-bottom: 30px;">
+                    <p style="font-family: 'Helvetica Neue', sans-serif;font-size: 20px;font-style: italic;font-weight: 700;line-height: 24px;letter-spacing: 0em;text-align: center;">${time}</p>
+                    <p style="font-family: 'Helvetica Neue', sans-serif;font-size: 20px;font-style: italic;font-weight: 400;line-height: 24px;letter-spacing: 0em;text-align: center;">${place}</p>
+                </div>
+
+                <div class="footer" style="font-family: Helvetica;width: 580px;height: 25px;margin: 0 auto;display: flex;justify-content: space-between;font-size: 15px;font-style: normal;font-weight: 400;line-height: 22px;letter-spacing: 0em;text-align: left;color: #6B6B6B;">
+                    <p>ССА</p>
+                    <p>Event</p>
+                    <p>${year}</p>
                 </div>
             </body>
-            </html>`
+
+            `
 
         const result = await sendMail(mail, "Регистрания на мероприятие: " + event, mailBody)
 
